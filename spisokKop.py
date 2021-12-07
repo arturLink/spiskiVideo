9523eee9b2546c0c3116af85b5a9a1f87bb45626
opilane <opilane@HP-CZC6297B8Z>
2021-12-07 14:36:08 +0200

    Add project files.

---
+++ spisokKop.py
@@ -0,0 +1,27 @@
+spisok=[]
+abc=['a', 'b', 'c']
+slovo="kokolad"
+slovo_list=list(slovo)
+print(slovo)
+print(slovo_list)
+while True :
+	print("1 add letter to a word")
+	print("2 link spiski")
+	print("3 to delete a letter from a word")
+	valik=int(input())
+	if valik==1:
+		a=input("Enter letter")
+		slovo_list.append(a)
+		print(f"Added {a}. New spisok", slovo_list)
+	elif valik==2:
+		slovo_list.extend(abc)
+		print(slovo_list)
+	elif valik==3:
+		a=input("Enter letter that is to be deleted")
+		n=slovo_list.count(a)
+		if n>0: 
+			for i in range(n):
+				slovo_list.remove(a)
+		else:
+			print("no letter")
+	print (slovo_list)
